efb5b554817baae8d4cff913b34efe11f2567049
Vũ Quang Tôn <vuquangton@outlook.com>
2026-08-18 14:38:14 +0700

    chore: ruff cleanup migration import

--- migrations/versions/b96454ef9dc8_add_currencies_exchange_rates_.py
+++ migrations/versions/b96454ef9dc8_add_currencies_exchange_rates_.py
@@ -5,9 +5,8 @@ Revises: 494sod_roles
 Create Date: 2026-08-18 14:36:04.282296
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = 'b96454ef9dc8'
